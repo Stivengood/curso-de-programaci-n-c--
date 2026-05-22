7d1aba9e1bad2fe720274a8299773eb1e57b8d38
Stiven <stivenjamimk2122@gmail.com>
2026-05-22 18:14:19 -0500

    creacion de la clase telefono

---
+++ aprendiendo java/ejercicio2/src/Telefono.java
@@ -0,0 +1,15 @@
+/*
+Autor: Stiven Jami
+Tarea: Ejercicio 2*/
+public class Telefono {
+    private String marca;
+    private String modelo;
+    private int precio;
+    public Telefono(String marca, String modelo, int precio){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+    
+
+}
