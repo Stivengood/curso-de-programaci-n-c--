f42e1f2140b8936799cd3a5ddb220032d120b2a3
Stiven <stivenjamimk2122@gmail.com>
2026-05-22 18:39:47 -0500

    telefono prueba

---
+++ aprendiendo java/ejercicio2/src/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Telefono telefono = new Telefono("iPhone", "17 pro max", 1.120);
+        System.out.println("---Bienvenido---");
+        telefono.mostrarInfo();
+    }
+}
