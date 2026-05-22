745dd4cc0e4d77f45a76efe609148817fc3515bf
Stiven <stivenjamimk2122@gmail.com>
2026-05-22 17:36:10 -0500

    ejercicio Mascota prueba y error

--- aprendiendo java/ejercicio1/src/Mascota.java
+++ aprendiendo java/ejercicio1/src/Mascota.java
@@ -2,5 +2,17 @@ public class Mascota {
     String nombre;
     String especie;
     int edad;
+    public Mascota(String nombre, String especie, int edad){
+    this.nombre = nombre;
+    this.especie = especie;
+    this.edad = edad;
+    }
+    public void mostrarInfo(){
+    System.out.println("Nombre:"+ this.nombre);
+    System.out.println("Especie: + this.especie");
+    System.out.println("Edad:" + this.edad);
+
+    }
+
 
 }
