de7b9d59f9f9ffc8e490e6f91821f40a27f49e6b
Stiven <stivenjamimk2122@gmail.com>
2026-05-22 19:28:42 -0500

    Clase telefono

--- aprendiendo java/ejercicio2/src/Telefono.java
+++ aprendiendo java/ejercicio2/src/Telefono.java
@@ -5,11 +5,39 @@ public class Telefono {
     private String marca;
     private String modelo;
     private int precio;
-    public Telefono(String marca, String modelo, int precio){
+    public Telefono(  String marca, String modelo, int precio){
         this.marca = marca;
         this.modelo = modelo;
-        this.precio = precio;
+        setPrecio(precio); 
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public int getPrecio(){
+        return precio;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public void setPrecio(){
+        if (precio >= 0 ) {
+            this.precio = precio;
+        }else{
+            System.out.println("Error: Valor ingresado invalido");
+        }
+
+
     }
     
+
+
 
 }
